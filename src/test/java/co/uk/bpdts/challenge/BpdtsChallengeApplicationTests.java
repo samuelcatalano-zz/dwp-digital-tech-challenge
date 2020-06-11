98244eaa89c4d18b4862bd08c8a29e504567e6a4
Samuel Catalano <samuel.catalano@gmail.com>
2020-06-11 22:24:21 +0100

    Submitting project to GitHub

---
+++ src/test/java/co/uk/bpdts/challenge/BpdtsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.bpdts.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpdtsChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
